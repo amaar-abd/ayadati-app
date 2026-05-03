e558e2dcc4f32f05d41039026acdc4b57278b4d3
Amaar Abd <abdamaar64@gmail.com>
2026-05-03 19:13:37 +0300

    feat: add Arabic localization support

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:ayadati/core/depandency_injection/service_locator.dart';
 import 'package:ayadati/core/routes/app_routes.dart';
 import 'package:ayadati/core/routes/route_generator.dart';
 import 'package:ayadati/core/theme/app_theme.dart';
+import 'package:ayadati/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   getItInit();
@@ -20,6 +22,14 @@ class Ayadati extends StatelessWidget {
       initialRoute: AppRoutes.splashView,
       theme: AppTheme.mainTheme,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates:const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            locale:const Locale('ar'),
     );
   }
 }
