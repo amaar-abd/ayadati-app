23fffb9d047e942a8aef01072c99fe78f092d49f
Amaar Abd <abdamaar64@gmail.com>
2026-05-04 10:04:00 +0300

    Add Firebase configuration to the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:ayadati/core/depandency_injection/service_locator.dart';
 import 'package:ayadati/core/routes/app_routes.dart';
 import 'package:ayadati/core/routes/route_generator.dart';
 import 'package:ayadati/core/theme/app_theme.dart';
+import 'package:ayadati/firebase_options.dart';
 import 'package:ayadati/generated/l10n.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
   getItInit();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Ayadati());
 }
 
@@ -22,14 +26,14 @@ class Ayadati extends StatelessWidget {
       initialRoute: AppRoutes.splashView,
       theme: AppTheme.mainTheme,
       debugShowCheckedModeBanner: false,
-      localizationsDelegates:const [
-                S.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: S.delegate.supportedLocales,
-            locale:const Locale('ar'),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
     );
   }
 }
