5dce26b11611cde59513d22c8d8fdb45bca8d9a0
Amaar Abd <abdamaar64@gmail.com>
2026-05-04 19:01:44 +0300

    chore(di): register FirebaseAuth, Firestore, DataSources, and Repos in GetIt

--- lib/core/depandency_injection/service_locator.dart
+++ lib/core/depandency_injection/service_locator.dart
@@ -1,7 +1,16 @@
+import 'package:ayadati/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:ayadati/features/auth/data/datasources/auth_remote_data_source_impl.dart';
+import 'package:ayadati/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:ayadati/features/auth/domain/repos/auth_repo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt sl = GetIt.instance;
 
 void getItInit() {
-
+  sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+  sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+  sl.registerLazySingleton<AuthRemoteDataSource>(() =>AuthRemoteDataSourceImpl(firestore: sl.get<FirebaseFirestore>(),firebaseAuth:  sl.get<FirebaseAuth>()));
+  sl.registerLazySingleton<AuthRepo>(() => AuthRepoImpl(authRemoteDataSource: sl.get<AuthRemoteDataSource>()));
 }
--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -1,4 +1,7 @@
 import 'package:ayadati/core/routes/app_routes.dart';
+import 'package:ayadati/features/auth/presentation/views/signin_view.dart';
+import 'package:ayadati/features/auth/presentation/views/signup_view.dart';
+import 'package:ayadati/features/home/presentation/views/home_view.dart';
 import 'package:ayadati/features/onboarding/presentation/view/onboarding_view.dart';
 import 'package:ayadati/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +13,12 @@ class RouteGenerator {
          return MaterialPageRoute(builder: (context) => const SplashView());
       case AppRoutes.onboardingView:
          return MaterialPageRoute(builder: (context) => const OnboardingView());
+      case AppRoutes.loginView:
+         return MaterialPageRoute(builder: (context) => const SigninView());
+      case AppRoutes.signupView:
+         return MaterialPageRoute(builder: (context) => const SignupView());
+      case AppRoutes.mainView:
+         return MaterialPageRoute(builder: (context) => const HomeView());
 
       default:
         return MaterialPageRoute(
