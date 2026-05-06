901be4c97299443c39ae31d336be0850cf55b63f
Amaar Abd <abdamaar64@gmail.com>
2026-05-06 22:14:56 +0300

    feat: integrate facebook login with UI button

--- lib/features/auth/presentation/widgets/signin_view_body.dart
+++ lib/features/auth/presentation/widgets/signin_view_body.dart
@@ -153,7 +153,9 @@ class _SigninViewBodyState extends State<SigninViewBody> {
                                 widget: Image.asset(Assets.assetsImagesMeta),
                                 text: 'فيسبوك',
                                 onTap: () {
-                                  //
+                                  context
+                                      .read<SocialauthCubit>()
+                                      .signInWithFacebook();
                                 },
                               ),
                             ],
