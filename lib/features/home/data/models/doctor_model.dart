4428a3f2df11d9dc9903068ed40723a01adc92ae
Amaar Abd <abdamaar64@gmail.com>
2026-05-09 15:25:01 +0300

    feat(data): implement DoctorModel with fromJson/toJson mapping

---
+++ lib/features/home/data/models/doctor_model.dart
@@ -0,0 +1,56 @@
+import 'package:ayadati/features/home/domain/entites/doctor_entity.dart';
+
+class Doctor extends DoctorEntity {
+  Doctor({
+    required super.name,
+    required super.image,
+    required super.desc,
+    required super.city,
+    required super.address,
+    required super.isrecommended,
+    required super.rating,
+    required super.fees,
+    required super.specialty,
+  });
+
+  factory Doctor.fromJson(Map<String, dynamic> json) {
+    return Doctor(
+      name: json['name']?? '',
+      image: json['image']?? '',
+      desc: json['desc']?? '',
+      city: json['city']?? '',
+      address: json['address']?? '',
+      isrecommended: json['is_recommended']?? false,
+      rating: json['rating']?.toString()??'0.0',
+      fees: json['fees']?? '',
+      specialty: json['specialty']?? '',
+    );
+  }
+  Map<String, dynamic> doctorToJson() {
+    return {
+      'name': name,
+      'image': image,
+      'desc': desc,
+      'city': city,
+      'address': address,
+      'is_recommended': isrecommended,
+      'rating': rating,
+      'fees': fees,
+      'specialty': specialty,
+    };
+  }
+
+  DoctorEntity toDoctorEntity() {
+    return DoctorEntity(
+      name: name,
+      image: image,
+      desc: desc,
+      city: city,
+      address: address,
+      isrecommended: isrecommended,
+      rating: rating,
+      fees: fees,
+      specialty: specialty,
+    );
+  }
+}
