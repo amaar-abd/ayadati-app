113dd0f04ed1a63006243d11b0fe6b0a0ab81d3a
Amaar Abd <abdamaar64@gmail.com>
2026-05-14 16:47:19 +0300

    feat(data): implement AppointmentsRemoteDataSource and its implementation

---
+++ lib/features/appointments/data/data_source/appointments_remote_data_source_impl.dart
@@ -0,0 +1,27 @@
+import 'package:ayadati/core/errors/custom_exception.dart';
+import 'package:ayadati/features/appointments/data/data_source/appointments_remote_data_source.dart';
+import 'package:ayadati/features/user_booking/data/models/appointment_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppointmentsRemoteDataSourceImpl implements AppointmentsRemoteDataSource {
+  AppointmentsRemoteDataSourceImpl({required this.firestore});
+  final FirebaseFirestore firestore;
+
+  @override
+  Future<List<AppointmentModel>> getRemoteAppointments(
+    String userId,
+    String path,
+  ) async {
+    try {
+      final querySnapshot = await firestore
+          .collection(path)
+          .where('userId', isEqualTo: userId).orderBy('createdAt', descending: true)
+          .get();
+      return querySnapshot.docs
+          .map((doc) => AppointmentModel.fromJson(doc.data()))
+          .toList();
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+}
