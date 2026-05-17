94657648c21e5951385d86661bc30bd803035113
Amaar Abd <abdamaar64@gmail.com>
2026-05-17 13:10:15 +0300

    feat(profile): create UpdateProfileUseCase

--- lib/features/profile/domain/use_cases/update_user_profile_use_case.dart
+++ lib/features/profile/domain/use_cases/update_user_profile_use_case.dart
@@ -3,11 +3,11 @@ import 'package:ayadati/core/common/domain/entites/user_entity.dart';
 import 'package:ayadati/features/profile/domain/repo/profile_repositotry.dart';
 import 'package:dartz/dartz.dart';
 
-class GetUserProfileUseCase {
-  GetUserProfileUseCase({required this.profileRepositotry});
+class UpdateUserProfileUseCase {
+  UpdateUserProfileUseCase({required this.profileRepositotry});
   final ProfileRepositotry profileRepositotry;
 
-  Future<Either<Failure, UserEntity>> call(String userId) async {
-   return await profileRepositotry.getUserProfile(userId);
+  Future<Either<Failure, void>> call(UserEntity userEntity) async {
+    return await profileRepositotry.updateUserProfile(userEntity);
   }
 }
