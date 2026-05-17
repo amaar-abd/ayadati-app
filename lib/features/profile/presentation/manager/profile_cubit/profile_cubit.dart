c1b42bf3368fa1aad38e979ba9640112d1c3d835
Amaar Abd <abdamaar64@gmail.com>
2026-05-17 13:10:40 +0300

    feat(profile): add updateData method in ProfileCubit to handle profile updates

--- lib/features/profile/presentation/manager/profile_cubit/profile_cubit.dart
+++ lib/features/profile/presentation/manager/profile_cubit/profile_cubit.dart
@@ -1,12 +1,15 @@
-import 'package:ayadati/features/auth/domain/entites/user_entity.dart';
+import 'package:ayadati/core/common/domain/entites/user_entity.dart';
 import 'package:ayadati/features/profile/domain/use_cases/get_user_profile_use_case.dart';
+import 'package:ayadati/features/profile/domain/use_cases/update_user_profile_use_case.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit(this.getUserProfileUseCase) : super(ProfileInitial());
+  ProfileCubit(this.getUserProfileUseCase, this.updateUserProfileUseCase)
+    : super(ProfileInitial());
 
   final GetUserProfileUseCase getUserProfileUseCase;
+  final UpdateUserProfileUseCase updateUserProfileUseCase;
 
   Future<void> getUserData(String userId) async {
     emit(ProfileLoading());
@@ -15,6 +18,16 @@ class ProfileCubit extends Cubit<ProfileState> {
     result.fold(
       (failure) => emit(ProfileError(message: failure.message)),
       (user) => emit(ProfileLoaded(user: user)),
+    );
+  }
+
+  Future<void> updateData(UserEntity userData) async {
+    emit(ProfileUpdateLoading());
+    final result = await updateUserProfileUseCase(userData);
+    result.fold(
+      (failure) => emit(ProfileUpdateError(message: failure.message)),
+      (success) =>
+          emit(ProfileUpdateSuccess(message: 'تم تحديث بيانات الملف الشخصي')),
     );
   }
 }
